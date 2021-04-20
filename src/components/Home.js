89bc71d774e1133db4fe745dc74d6e03592728e2
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2021-04-19 18:22:32 -0600

    add post index component

---
+++ src/components/Home.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import './Home.css';
+
+const Home = () => {
+  const [posts, setPosts] = useState(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('http://localhost:3000/posts', {
+        mode: 'cors',
+      });
+      const data = await response.json();
+      const item = data.posts;
+      console.log(item);
+      const listPosts = item.map((post) => {
+        return <li key={post.title}>{post.title}</li>;
+      });
+      setPosts(listPosts);
+    };
+    fetchPosts();
+  }, []);
+
+  return (
+    <div className="mainContent">
+      <ul className="postIndex">{posts ? posts : ''}</ul>
+    </div>
+  );
+};
+
+export default Home;
