b989bb0db155c102e12c9df59a830c9c16a5ba6e
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2021-05-10 13:51:49 -0600

    Add error on failed login

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,6 +7,7 @@ const Login = (props) => {
   const [username, setUsername] = useState(null);
   const [password, setPassword] = useState(null);
   const [redirect, setRedirect] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -22,9 +23,13 @@ const Login = (props) => {
     fetch('http://localhost:3000/login', requestOptions)
       .then((response) => response.json())
       .then((results) => {
-        window.localStorage.setItem('user', results.token);
-        props.authRefresh(true);
-        setRedirect(true);
+        if (results.token !== undefined) {
+          window.localStorage.setItem('user', results.token);
+          props.authRefresh(true);
+          setRedirect(true);
+        } else {
+          setErrorMessage(results.message);
+        }
       });
   };
 
@@ -56,6 +61,7 @@ const Login = (props) => {
           />
           <br />
           <input type="submit" value="Login" />
+          {errorMessage && <div>{errorMessage}</div>}
         </form>
       </div>
     </Layout>
