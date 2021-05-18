0da84f520baa1b97bd14ab5b8a64382495505c88
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2021-05-18 11:22:41 -0600

    Add correct basename

--- src/Routes.js
+++ src/Routes.js
@@ -45,7 +45,7 @@ const Routes = () => {
 
   return (
     <BrowserRouter
-      basename={'https://serene-waters-04286.herokuapp.com'}
+      basename={'https://cameronstamant.github.io/blog-api-client/'}
       forceRefresh
     >
       <Switch>
