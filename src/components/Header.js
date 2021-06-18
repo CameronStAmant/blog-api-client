a7e4835a53e97855fbf0f2a11119d84c4dff26ed
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2021-06-18 09:04:53 -0600

    Update header to use tailwind

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,33 @@
-import './Header.css';
 import { NavLink } from 'react-router-dom';
 
 const Header = (props) => {
   return (
-    <div className="header">
-      <ul className="headerNav">
-        <li>
-          <NavLink to="/">Home</NavLink>
+    <div className="display: grid grid-cols-3 col-span-3 bg-green-100">
+      <ul className="display: grid col-start-1 col-span-full grid-flow-col place-items-center">
+        <li className="border-solid border-4 border-green-200 rounded-md bg-green-200 hover:bg-green-300 hover:border-green-300 w-5/6 active:bg-green-400 active:border-green-400 shadow-sm">
+          <NavLink to="/" className="display: block">
+            Home
+          </NavLink>
         </li>
         {!props.authState && (
-          <li>
-            <NavLink to="/login">Login</NavLink>
+          <li className="sm:col-start-3 lg:col-start-4 border-solid border-4 border-green-200 rounded-md bg-green-200 hover:bg-green-300 hover:border-green-300 w-5/6 active:bg-green-400 active:border-green-400 shadow-sm">
+            <NavLink to="/login" className="display: block">
+              Login
+            </NavLink>
           </li>
         )}
         {!props.authState && (
-          <li>
-            <NavLink to="/signup">Sign up</NavLink>
+          <li className="sm:col-start-4 lg:col-start-5 border-solid border-4 border-green-200 rounded-md bg-green-200 hover:bg-green-300 hover:border-green-300 w-5/6 active:bg-green-400 active:border-green-400 shadow-sm">
+            <NavLink to="/signup" className="display: block">
+              Sign up
+            </NavLink>
           </li>
         )}
         {props.authState && (
-          <li>
-            <NavLink to="/logout">Log out</NavLink>
+          <li className="sm:col-start-4 lg:col-start-5 border-solid border-4 border-green-200 rounded-md bg-green-200 hover:bg-green-300 hover:border-green-300 w-5/6 active:bg-green-400 active:border-green-400 shadow-sm">
+            <NavLink to="/logout" className="display: block">
+              Log out
+            </NavLink>
           </li>
         )}
       </ul>
