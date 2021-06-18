b7aae05827259355f82db6db5d09813ff45286b8
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2021-06-18 09:04:16 -0600

    Update footer to use tailwind

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,7 @@
-import './Footer.css';
-
 const Footer = () => {
   return (
-    <div className="footer">
-      <ul className="footerNav">
+    <div className="display: grid row-start-3 col-span-full">
+      <ul className="display: grid grid-flow-col">
         <li>Privacy</li>
         <li>About</li>
         <li>Contact us</li>
