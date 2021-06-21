8ce1fd96b66409cea29446e714cfa53d7516ec03
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2021-06-21 07:46:38 -0600

    Update Comments to use tailwind

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import './Comments.css';
 const { DateTime } = require('luxon');
 
 const Comments = (props) => {
@@ -18,14 +17,17 @@ const Comments = (props) => {
       let array = [];
       for (const comment of data.comments) {
         const element = (
-          <div className="commentLayout" key={comment._id}>
-            <h4 className="commentAuthor">{comment.author.username}</h4>
-            <p className="commentTimestamp">
+          <div
+            className="box-border border-solid m-2 shadow-md p-1 display: grid gap-2 grid-rows-2 grid-cols-2 auto-rows-min-content justify-self-stretch border-2 border-green-200 rounded-md "
+            key={comment._id}
+          >
+            <h4 className="justify-self-start">{comment.author.username}</h4>
+            <p className="justify-self-end">
               {DateTime.fromISO(comment.timestamp).toLocaleString(
                 DateTime.DATETIME_MED
               )}
             </p>
-            <p className="commentBody">{comment.body}</p>
+            <p className="row-start-2 col-span-full">{comment.body}</p>
             <br />
           </div>
         );
@@ -35,7 +37,7 @@ const Comments = (props) => {
     };
     fetchComments();
   }, [props.loadComments]);
-  return <div className="Comment">{props.comments}</div>;
+  return <div className="justify-self-stretch my-14">{props.comments}</div>;
 };
 
 export default Comments;
