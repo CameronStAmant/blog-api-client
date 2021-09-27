1e12474cdb682c941ccd65ad136a89cea43f3641
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2021-09-27 07:33:32 -0600

    Standardize spacing between buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import NavButton from './NavButton';
 const Header = (props) => {
   return (
     <div className="grid grid-cols-3 row-start-1 bg-gradient-to-b from-green-900 to-green-400 py-2 text-white text-center text-lg">
-      <ul className="flex gap-4 col-span-full grid-flow-col place-items-center mx-4">
+      <ul className="flex space-x-3 col-span-full grid-flow-col place-items-center mx-4">
         <NavButton link="/" text="Home" />
         {!props.authState && <NavButton link="/login" text="Login" />}
         {!props.authState && <NavButton link="/signup" text="Sign up" />}
