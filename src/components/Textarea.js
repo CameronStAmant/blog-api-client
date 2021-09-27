9ae32ae387824dde2b138993b2bd8418724e82ff
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2021-09-27 14:19:45 -0600

    Update Textarea and favicons

--- src/components/Textarea.js
+++ src/components/Textarea.js
@@ -2,7 +2,7 @@ function Textarea(props) {
   return (
     <textarea
       className="box-border border-2 shadow-sm rounded-md gap-4 border-cyan auto-rows-min w-full block mb-2"
-      rows="12"
+      rows="4"
       name="body"
       value={props.value ? props.value : ''}
       onChange={props.onChange}
