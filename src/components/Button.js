06a27bcb09dfb75de01d7fe4072842a39d1a2408
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2021-09-27 07:28:25 -0600

    Add standard font size to buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ function Button(props) {
   return (
     <button
       onClick={props.onClick}
-      className={`border-solid border-4 border-${props.color}-200 rounded-md bg-${props.color}-200 hover:bg-${props.color}-300 hover:border-${props.color}-300 active:bg-${props.color}-400 active:border-${props.color}-400 shadow-sm ${props.addonClasses} px-md`}
+      className={`border-solid border-4 border-${props.color}-200 rounded-md bg-${props.color}-200 hover:bg-${props.color}-300 hover:border-${props.color}-300 active:bg-${props.color}-400 active:border-${props.color}-400 shadow-sm ${props.addonClasses} px-md text-base`}
       type={props.type ? props.type : 'button'}
     >
       {props.value}
