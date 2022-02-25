4aa4487329bfd34860eb096b545c0a117833f37b
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2022-02-25 08:10:16 -0700

    Update heroku url

--- src/const.js
+++ src/const.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000'
-    : 'https://serene-waters-04286.herokuapp.com';
+    : 'https://rocky-coast-78937.herokuapp.com';
 
 export default baseUrl;
